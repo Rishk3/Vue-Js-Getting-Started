4a6ce999401062ed91a756e97c036555414b0436
Rishk3 <anchalesh10@gmail.com>
2021-02-07 10:04:51 +0530

    links changed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link>
-    <router-link to="/weather">GetWeather</router-link>
     <router-link to="/login">Login</router-link>
     <router-link to="/register">Register</router-link>
     <router-link to="/dashboard">Dashboard</router-link>
